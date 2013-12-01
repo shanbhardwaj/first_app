d418d60bbebbcb4658ce91cdadfd8a4dd2b288fe
Shantanu Bhardwaj <bhardwaj.10@gmail.com>
2013-12-01 11:56:07 +0530

    f4 changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,5 @@ class ApplicationController < ActionController::Base
   # a new comment forfeature 
   # a new comment for feature f3
   # a minor change to complete release v1.2
+  # feature f4 started
 end
