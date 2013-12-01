68b4381d16aa0295fd8f0dcfdfcb9c5a483684f6
Shantanu Bhardwaj <bhardwaj.10@gmail.com>
2013-12-01 11:57:31 +0530

    f5 complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  # change for f5
   # some methods that get included by all controllers
   # a new comment forfeature 
   # a new comment for feature f3
   # a minor change to complete release v1.2
+  
 end
